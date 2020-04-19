3163483b9d6624ba03aef8c0438ac4d5eff1b22a
Jakub Ziemann <ziemannjakub@gmail.com>
2020-04-19 21:08:45 +0000

    step1

--- part1/courseInfo/courseinfo/src/index.js
+++ part1/courseInfo/courseinfo/src/index.js
@@ -1,7 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const Header = (props) => {
+  return (
+    <h1>{props.name}</h1>
+  )
+}
+
+const Content = (props) => {
+  return (
+      <p>{props.name} {props.number}</p> 
+  )
+}
+
+const Total = (props) => {
+  return(
+
+      <p>Number of exercises = {props.totalNumber}</p>
+  )
+}
+
 const App = () => {
+  // const definitions
   const course = 'Half Stack application development'
   const part1 = "Fundamentals of React"
   const exercise1 = 10
@@ -12,16 +32,11 @@ const App = () => {
 
   return (
     <div>
-      <p>
-        {part1} {exercise1}
-      </p>
-      <p>
-        {part2} {exercise2}
-      </p>
-      <p>
-        {part3} {exercise3}
-      </p>
-  <p>Number of exercises {exercise1 + exercise2 + exercise3}</p>
+      <Header name={course} />
+      <Content name = {part1} number={exercise1}/>
+      <Content name = {part2} number={exercise2}/>
+      <Content name = {part3} number={exercise3}/>
+      <Total totalNumber = {exercise1 + exercise2 + exercise3}/>
     </div>
    )
 }
