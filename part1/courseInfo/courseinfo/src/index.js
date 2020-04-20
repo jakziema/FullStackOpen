98fcdddebc6e1630026c36bfaa0a95f92730b7eb
Jakub Ziemann <ziemannjakub@gmail.com>
2020-04-20 14:57:22 +0000

    Event handlers

--- part1/courseInfo/courseinfo/src/index.js
+++ part1/courseInfo/courseinfo/src/index.js
@@ -4,14 +4,26 @@ import ReactDOM from 'react-dom'
 const App = (props) => {
   const [ counter, setCounter ] = useState(0)
 
-  setTimeout(
-    () => setCounter(counter + 1),
-    1000
-  )
-    console.log('rendering...', counter)
+  // const handleClick = () => {
+  //   console.log("Clicked")
+    
+  // }
+
+  const increaseByOne = () => setCounter(counter + 1)
+  const setToZero = () => setCounter(0)
+
   return (
-    <div>{counter}</div>
+    <div>
+      <div>{counter} </div>
+        <button onClick={increaseByOne}>
+        plus
+        </button>
+        <button onClick= {setToZero}>
+          zero
+        </button>
+    </div>
   )
+  
 }
 
 ReactDOM.render(
