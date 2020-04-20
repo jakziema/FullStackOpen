370d64978b47576a87e5aa2404eae1c31468debb
Jakub Ziemann <ziemannjakub@gmail.com>
2020-04-20 20:01:37 +0000

    step1 done

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,17 +1,64 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const Header = ({titleString}) => {
+  return (
+      <h1>
+      {titleString}
+      </h1>
+    
+  )
+}
+
+const StatisticsHeader = ({title, stat}) => {
+  return (
+    <p>
+      {title} : {stat}
+    </p>
+  )
+}
+const Button = ({onClick, text}) => {
+  return (
+    <button onClick = {onClick}>
+      {text}
+    </button>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const setGoodClick = () => {
+    setGood(good + 1)
+  }
+
+  const setNeutralClick = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const setBadClick = () => {
+    console.log(bad)
+    setBad(bad + 1)
+  }
+
+
+  return (
+    <div>
+      <Header titleString = "give feedback"/>
+      <Button onClick = {setGoodClick} text = 'good'/>
+      <Button onClick = {setNeutralClick} text = 'neutral'/>
+      <Button onClick = {setBadClick} text = 'bad'/>
+      <Header titleString = "statistics"/>
+      <StatisticsHeader title = 'good'  stat = {good}/>
+      <StatisticsHeader title = 'neutral'  stat = {neutral}/>
+      <StatisticsHeader title = 'bad'  stat = {bad}/>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, 
   document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+)
