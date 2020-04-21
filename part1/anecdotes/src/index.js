349c80dcdfaa29274942a35a0af2c022a049617e
Jakub Ziemann <ziemannjakub@gmail.com>
2020-04-21 19:17:15 +0000

    step2 done

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,24 +1,53 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Button = ({onClick, text}) => {
+const Button = ({ onClick, text }) => {
   return (
     <p>
-    <button onClick = {onClick}>
-      {text}
-    </button>
+      <button onClick={onClick}>
+        {text}
+      </button>
     </p>
   )
 }
 
 const App = (props) => {
+  //make empty array with a size of anecdotes array
+  const voteList = Array(props.anecdotes.length).fill(0)
   const [selected, setSelected] = useState(0)
+  //
+  const [vote, setVote] = useState([...voteList])
   
+  console.log(vote)
+
+  
+
+  const randomClick = () => {
+    const randomNumber = Math.floor(Math.random() * (anecdotes.length - 1))
+    setSelected(randomNumber)
+  }
+
+  const voteClick = () => {
+    //copy of vote array
+    const copy = [...vote]
+    //selected vote in copy array add vote
+    copy[selected] += 1
+    //update vote array
+    setVote(copy)
+    
+    
+  }
+
+
   return (
     <div>
-      <p>{anecdotes[selected]}</p>
-  <Button onClick ={() => setSelected(Math.floor(Math.random() * (anecdotes.length - 1))) } text = 'next anecdote' />
+      <h1>{props.anecdotes[selected]}</h1>
+      <p>has {vote[selected]}</p>
+      <Button onClick = {voteClick} text = "vote" />
+      <Button onClick={randomClick} text='next anecdote' />
+      
     </div>
+
   )
 }
 
@@ -30,6 +59,9 @@ const anecdotes = [
   'Premature optimization is the root of all evil.',
   'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
 ]
+
+
+
 
 ReactDOM.render(
   <App anecdotes={anecdotes} />,
