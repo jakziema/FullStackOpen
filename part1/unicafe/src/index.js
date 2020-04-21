4cdd14c5039bc9d3ee4e5c533e31332f69efbfe8
Jakub Ziemann <ziemannjakub@gmail.com>
2020-04-21 14:07:48 +0000

    step6 done

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,33 +1,42 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Header = ({titleString}) => {
+const Header = ({text}) => {
   return (
-      <h1>
-      {titleString}
-      </h1>
+      <div>
+      <h1>{text}</h1>
+      </div>
     
   )
 }
 
-const StatisticsHeader = ({title, stat}) => {
+const Statistic =  ({text, value}) => {
+  return (
+    <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
+  )
   
-    if (title === 'positive') {
-      return (
-        <p>
-      {title} : {stat} %
-    </p>
-      )
-    } return (
-      <p>
-        {title} : {stat}
-      </p>
-      
-    )
-      
-    
-    
   
+}
+
+const Statistics = ({statistics}) => {
+  const hasFeedback = statistics.allVotes.value > 0
+  const showFeedback = (
+    <table>
+      <tbody>
+        {Object.values(statistics).map(stat => (
+          <Statistic key={stat.text} text={stat.text} value={stat.value} />
+        ))}
+      </tbody>
+    </table>
+  )
+
+  const showNoFeedback = <p>No feedback given</p>
+
+        return <div>{hasFeedback ? showFeedback: showNoFeedback}</div>
+   
 }
 const Button = ({onClick, text}) => {
   return (
@@ -37,48 +46,41 @@ const Button = ({onClick, text}) => {
   )
 }
 
+
+
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [allVotes, setAll] = useState(0)
-  const [allScore,setAllScore] = useState(0)
 
-  const setGoodClick = () => {
-    setGood(good + 1)
-    setAll(allVotes +1)
-    setAllScore(allScore + 1)
+  const allScore = good + neutral - bad
+  const allVotes = good + neutral + bad
+  const average = allScore / allVotes
+  const positive = (good * 100 / allVotes).toFixed(2)
+
+  //object key: value
+  const statistics = {
+    good: { text: 'Good', value: good },
+    neutral: { text: 'Neutral', value: neutral },
+    bad: { text: 'Bad', value: bad },
+    allVotes: { text: 'All', value: allVotes },
+    average: { text: 'Average', value: average },
+    positive: { text: 'Positive', value: positive }
   }
-
-  const setNeutralClick = () => {
-    setNeutral(neutral + 1)
-    setAll(allVotes +1)
-  }
-
-  const setBadClick = () => {
-    console.log(bad)
-    setBad(bad + 1)
-    setAll(allVotes +1)
-    setAllScore(allScore - 1)
-  }
-
-
-
 
   return (
     <div>
-      <Header titleString = "give feedback"/>
-      <Button onClick = {setGoodClick} text = 'good'/>
-      <Button onClick = {setNeutralClick} text = 'neutral'/>
-      <Button onClick = {setBadClick} text = 'bad'/>
-      <Header titleString = "statistics"/>
-      <StatisticsHeader title = 'good'  stat = {good}/>
-      <StatisticsHeader title = 'neutral'  stat = {neutral}/>
-      <StatisticsHeader title = 'bad'  stat = {bad}/>
-      <StatisticsHeader title = 'all' stat = {allVotes} />
-      <StatisticsHeader title = 'average' stat = {allScore / allVotes} />
-      <StatisticsHeader title = 'positive' stat = {good * 100 / allVotes } />
+      <Header text = "Give feedback"/>
+      <Button onClick = {() => setGood(good + 1) } text = 'good'/>
+      <Button onClick = {() => setNeutral( neutral + 1)} text = 'neutral'/>
+      <Button onClick = {() => setBad(bad + 1)} text = 'bad'/>
+      {console.log(statistics.good.value)}
+      <Header text = "Statistics"/>
+      <Statistics statistics = {statistics}/>
+       
+
+      
     </div>
   )
 }
